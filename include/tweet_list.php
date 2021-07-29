7391398934528c2f6c19357dd800e41fd4b99070
LorentMaxime <lorent.max@gmail.com>
2021-07-29 15:29:56 +0200

    UPDATE factorisation de la partie de code affichant les tweets (toute la partie boucle foreach et le code HTMl)

---
+++ include/tweet_list.php
@@ -0,0 +1,43 @@
+<!-- factorisation de la partie affichage des tweets -->
+<!-- avec la boucle foreach. ATTTENTION à utiliser le nom de variable $TWEETS pour recuperer les données -->
+
+<?php
+
+foreach ($tweets as $tweet) :
+    ?>
+
+    <article class="media box">
+        <figure class="media-left">
+            <p class="image is-64x64">
+                <img src="https://bulma.io/images/placeholders/128x128.png" alt="image">
+            </p>
+        </figure>
+        <div class="media-content">
+            <div class="content">
+                <p>
+                    <a href="detailProfil.php?user_id=<?= $tweet['author_id'] ?>">
+                        <strong><?= $tweet['pseudo'] ?></strong>
+                    </a>
+                    <br>
+                    <?= $tweet['message'] ?>
+                </p>
+            </div>
+            <nav class="level is-mobile">
+                <div class="level-left">
+                    <a class="level-item">
+                        <span class="icon is-small"><i class="fas fa-reply"></i></span>
+                    </a>
+                    <a class="level-item">
+                        <span class="icon is-small"><i class="fas fa-retweet"></i></span>
+                    </a>
+                    <a class="level-item">
+                        <span class="icon is-small"><i class="fas fa-heart"></i></span>
+                    </a>
+                </div>
+            </nav>
+        </div>
+    </article>
+
+<?php
+endforeach;
+?>
